9deaf321a78120058507d09ea5e3eb45d7abc9b3
RunGeunLab <132816679+rungeun@users.noreply.github.com>
2025-04-28 12:25:47 +0900

    feat: 총알 스포너

--- Assets/BulletSpawner.cs
+++ Assets/BulletSpawner.cs
@@ -4,7 +4,7 @@ public class BulletSpawner : MonoBehaviour
 {
     public GameObject bulletPerfab;
     public float spawnRateMin = 0.5f;
-    public float spawnRatrMax = 3f;
+    public float spawnRateMax = 3f;
 
     private Transform target;
     private float spawnRate;
@@ -12,12 +12,25 @@ public class BulletSpawner : MonoBehaviour
 
     void Start()
     {
-        
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = FindFirstObjectByType < PlayerController>().transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeAfterSpawn += Time.deltaTime;
+
+        if(timeAfterSpawn >= spawnRate)
+        {
+            timeAfterSpawn = 0f;
+
+            GameObject bullet = Instantiate(bulletPerfab, transform.position, transform.rotation);
+
+            bullet.transform.LookAt(target);
+
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        }
     }
 }
